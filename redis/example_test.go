4a49a18f06892a5b02d877594dd90a5e4b85cc1d
Rahmad Afandi <rahmadafandinara@gmail.com>
2026-06-09 16:55:13 +0700

    docs(redis): add runnable godoc example for NewStorage

---
+++ redis/example_test.go
@@ -0,0 +1,29 @@
+// Copyright 2026 Rahmad Afandi. MIT License.
+
+package redis_test
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/alicebob/miniredis/v2"
+	goredis "github.com/redis/go-redis/v9"
+	fibrredis "github.com/rahmadafandi/fibr/redis"
+)
+
+// NewStorage adapts a go-redis client to fiber.Storage, e.g. for a
+// multi-instance rate limiter. miniredis stands in for real Redis here.
+func ExampleNewStorage() {
+	mr, _ := miniredis.Run()
+	defer mr.Close()
+
+	client := goredis.NewClient(&goredis.Options{Addr: mr.Addr()})
+	store := fibrredis.NewStorage(client, fibrredis.WithPrefix("ratelimit:"))
+
+	_ = store.Set("1.2.3.4", []byte("1"), time.Minute)
+	v, _ := store.Get("1.2.3.4")
+
+	fmt.Println(string(v))
+	// Output:
+	// 1
+}
